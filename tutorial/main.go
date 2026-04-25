b2a7f2552a775e10b592ef6de814ca3d07f8be7e
Abraham Samuel {JavaScript Enthusiast} <abmercy035@gmail.com>
2026-04-25 18:56:51 +0100

    Go:Variables - float

--- tutorial/main.go
+++ tutorial/main.go
@@ -24,19 +24,26 @@ func main() {
 
 //ints
 
-var ageOne int = 20
-var ageTwo = 30
-ageThree := 40
+// var ageOne int = 20
+// var ageTwo = 30
+// ageThree := 40
 
-fmt.Println(ageOne, ageTwo, ageThree)
+// fmt.Println(ageOne, ageTwo, ageThree)
 
 //bits and memory
 
-var numOne int8 = 25
-var numTwo int8 = -128
-var numThree uint16 = 256
+// var numOne int8 = 25
+// var numTwo int8 = -128
+// var numThree uint16 = 256
 
-fmt.Println(numOne, numTwo, numThree)
+// fmt.Println(numOne, numTwo, numThree)
 
+// float
+
+var scoreOne float32 = 25.98
+var scoreTwo float64 = 253465772345672243233333333.7
+scoreThree := 1.5
+
+fmt.Println(scoreOne, scoreTwo, scoreThree)
 
 }
