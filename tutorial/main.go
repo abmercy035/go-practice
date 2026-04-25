99f8810642ca26f605574b20753661266aa834e0
Abraham Samuel {JavaScript Enthusiast} <abmercy035@gmail.com>
2026-04-25 18:47:21 +0100

    Go:Variables - string

--- tutorial/main.go
+++ tutorial/main.go
@@ -4,4 +4,25 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+
+// strings
+
+var nameOne string  = "45"
+var nameTwo = "auto string"
+var nameThree string
+
+fmt.Println(nameOne, nameTwo, nameThree)
+
+nameOne ="peach"
+nameThree = "broswer"
+
+fmt.Println(nameOne, nameTwo, nameThree)
+
+nameFour := "fourthString"
+
+fmt.Println(nameFour)
+
+
+git
+
 }
